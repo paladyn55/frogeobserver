13f7cc1495d3fa21fcd1cdcd1db2686707199459
paladyn55 <josephbcassidy@gmail.com>
2022-10-23 00:07:05 +0100

    started image loading

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #1 - import dependencies
+from traceback import FrameSummary
 from sklearn.metrics import accuracy_score
 import numpy as np
 import os
@@ -8,5 +9,18 @@ from PIL import Image
 
 
 #2 - load data
+fdir = '/home/blop/frogeobserver/sorted/frogs'
+tdir = '/home/blop/frogeobserver/sorted/toads'
+farr = []
+tarr = []
+for i in os.listdir(fdir):
+    imgpath = os.path.join(fdir, i)
+    npa = img.imread(imgpath)
+    farr.append(npa)
+for i in os.listdir(tdir):
+    imgpath = os.path.join(tdir, i)
+    npa = img.imread(imgpath)
+    tarr.append(npa)
+    
 #3 - build and compile model
 #4 - fir predict and evaluate
