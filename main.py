c49616f68715d33b248a4037defbcf53240ca49a
paladyn55 <josephbcassidy@gmail.com>
2022-10-22 23:40:21 +0100

    added imports

---
+++ main.py
@@ -0,0 +1,12 @@
+#1 - import dependencies
+from sklearn.metrics import accuracy_score
+import numpy as np
+import os
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+#2 - load data
+#3 - build and compile model
+#4 - fir predict and evaluate
